c07fd3c4b90f4bdf48ab9a8fa1cd8ccd024efbcc
fiiuliana <fiiulianaf@gmail.com>
2021-05-30 20:04:57 +0300

    Added ProductViewModel with controller changes

---
+++ ClientApp/src/app/prducts/prducts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prducts',
+  templateUrl: './prducts.component.html',
+  styleUrls: ['./prducts.component.css']
+})
+export class PrductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
